c2b1d33315de93c42e6383f60b2c5f0c2a7614ad
Arnaud P <arnaud@xpressive.io>
2024-05-14 01:11:45 +0200

    Added base Movie model

---
+++ database/migrations/2024_05_13_143501_create_movies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->integer('tmdb_id');
+            $table->string('title');
+            $table->text('overview')->nullable();
+            $table->string('poster')->nullable();
+            $table->string('cover')->nullable();
+            $table->timestamp('release_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
